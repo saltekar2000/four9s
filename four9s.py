91a7ebd81c27af83f398308c0296ed3fe2a8025b
Shirish Altekar <saltekar@gmail.com>
2017-09-12 22:33:41 -0700

    complete solution - simplified but takes longer

--- four9s.py
+++ four9s.py
@@ -1,74 +1,64 @@
 import math
 
 four9s_log = dict()
+call_count = 0
 
-nines = [(9,1),(99,2),(999,3),(9999,4),
-         (.9,1),(.99,2),(.999,3),(.9999,4),
-            (9.9,2),(9.99,3),(9.999,4),
-                (99.9,3),(99.99,4),
-                     (999.9,4)]
-         
-
-def four9s_solver( flag = False, stack = [], num9s = 0, oplist = [] ):
+#nines_list = [(9,1),(99,2),(999,3),(9999,4),(.9,1),(.99,2),(.999,3),(.9999,4),(9.9,2),
+#              (9.99,3),(9.999,4),(99.9,3),(99.99,4),(999.9,4)]
+nines_list = [(9,1),(99,2),(.9,1),(.99,2),(9.9,2)]
+          
+def four9s_solver( stack = [], num9s = 0, oplist = [] ):
+    global call_count
+    call_count += 1
 
     # generic condition to limit search depth
     if num9s > 4 or len(oplist) > 12:
         return
 
-    if num9s == 4 and len(stack) is 1 and abs(float(stack[-1])-int(float(stack[-1]))) < 1e-6 and float(stack[-1]) >= 0 and float(stack[-1]) <= 100:
+    if (num9s == 4 and len(stack) is 1 and abs(stack[-1]-round(stack[-1])) < 1e-10 
+            and stack[-1] >= 0 and stack[-1] <= 100):
         opstr = ','.join(oplist)
         idx = int(float(stack[-1]))
+        #print( opstr, ' : ', idx )
         if not idx in four9s_log:
             four9s_log[idx] = opstr
         else:
-            if four9s_log[idx].count(',')-four9s_log[idx].count('ENTER') > opstr.count(',')-opstr.count('ENTER'):
+            if four9s_log[idx].count(',') > opstr.count(','):
                 four9s_log[idx] = opstr
         return
             
-    # 9
-    if flag is False:
-        four9s_solver( True, stack+['9'], num9s+1, oplist+['9'] )
-    else:
-        four9s_solver( True, stack[:-1]+[stack[-1]+'9'], num9s+1, oplist+['9'] )
+    # enter number 
+    for (nines_entry, additional_num9s) in nines_list:
+        four9s_solver( stack+[nines_entry], num9s+additional_num9s, oplist+[str(nines_entry)] )
 
-    # .9
-    if flag is False:
-        four9s_solver( True, stack+['.9'], num9s+1, oplist + ['.9'] )
-    elif stack[-1].count('.') == 0:
-        four9s_solver( True, stack[:-1]+[stack[-1]+'.9'], num9s+1, oplist+['.9'] )
-    
-    # ENTER
-    if flag is True:
-        four9s_solver( False, stack[:-1]+[float(stack[-1])], num9s, oplist+['ENTER'] )
-    
     # +
-    if len(stack) >= 2 and not oplist[-1] == 'ENTER':
-        four9s_solver( False, stack[:-2]+[stack[-2]+float(stack[-1])], num9s, oplist+['+'] )
+    if len(stack) >= 2:
+        four9s_solver( stack[:-2]+[stack[-2]+stack[-1]], num9s, oplist+['+'] )
     
     # - 
-    if len(stack) >= 2 and not oplist[-1] == 'ENTER':
-        four9s_solver( False, stack[:-2]+[stack[-2]-float(stack[-1])], num9s, oplist+['-'] )
+    if len(stack) >= 2:
+        four9s_solver( stack[:-2]+[stack[-2]-stack[-1]], num9s, oplist+['-'] )
     
     # *
-    if len(stack) >= 2 and not oplist[-1] == 'ENTER':
-        four9s_solver( False, stack[:-2]+[stack[-2]*float(stack[-1])], num9s, oplist+['*'] )
+    if len(stack) >= 2:
+        four9s_solver( stack[:-2]+[stack[-2]*stack[-1]], num9s, oplist+['*'] )
     
     # /
-    if len(stack) >= 2 and not float(stack[-1]) == 0 and not oplist[-1] == 'ENTER':
-        four9s_solver( False, stack[:-2]+[stack[-2]/float(stack[-1])], num9s, oplist+['/'] )
+    if len(stack) >= 2 and not stack[-1] == 0:
+        four9s_solver( stack[:-2]+[stack[-2]/stack[-1]], num9s, oplist+['/'] )
     
     # ^
-    if (len(stack) >= 2 and stack[-2] > 0 and stack[-2] < 10 and float(stack[-1]) > 0 and float(stack[-1]) < 10):
-        four9s_solver( False, stack[:-2]+[stack[-2]**float(stack[-1])], num9s, oplist+['^'] )
+    if (len(stack) >= 2 and stack[-2] > 0 and stack[-2] < 10 and stack[-1] > 0 and stack[-1] < 10):
+        four9s_solver( stack[:-2]+[stack[-2]**stack[-1]], num9s, oplist+['^'] )
     
     # FACTorial
-    if (len(stack) >= 1 and float(stack[-1]).is_integer() and float(stack[-1]) > 2 
-            and float(stack[-1]) < 20 and not oplist[-1] == 'ENTER'):
-        four9s_solver( False, stack[:-1] + [math.factorial(float(stack[-1]))], num9s, oplist+['FACT'] )
+    if (len(stack) >= 1 and float(stack[-1]).is_integer() and stack[-1] > 2 
+            and stack[-1] < 10 ):
+        four9s_solver( stack[:-1] + [math.factorial(stack[-1])], num9s, oplist+['FACT'] )
     
     # SQRT
-    if len(stack) >= 1 and float(stack[-1]) > 1 and not oplist[-1] == 'ENTER': 
-        four9s_solver( False, stack[:-1] + [math.sqrt(float(stack[-1]))], num9s, oplist+['SQRT'] )
+    if len(stack) >= 1 and stack[-1] > 1: 
+        four9s_solver( stack[:-1] + [math.sqrt(stack[-1])], num9s, oplist+['SQRT'] )
     
 
 if __name__ == '__main__':
@@ -79,6 +69,6 @@ if __name__ == '__main__':
             print(x, ':', four9s_log[x])
         else:
             print(x, ': NOT FOUND')
-    
+    print( call_count )
     
             
